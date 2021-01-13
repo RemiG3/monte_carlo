41f63fa8d272a2f1bf54a3c980a8673a3f036f67
guenole <55542044+yrhden@users.noreply.github.com>
2021-01-13 17:17:20 +0100

    Méthode 1

--- Question1/Methode1/Main.java
+++ Question1/Methode1/Main.java
@@ -1,11 +1,19 @@
-
-
-class Main{
-	
-	
-	public static void main(String[] args){
-		
-	}
-	
+public class Main
+{
+    
+    public double a = 0;
+    public double b = 1;
+    public double sum = 0;
+    
+    public long n = 10000000;
+    
+    public Main()
+    {
+        for(long i = 0; i < n; i++){
+            double rng = Math.random() * ( b - a );
+            sum += Math.sqrt(1 - (rng*rng));
+        }
+        double reply = (b - a) * (sum / n);
+        System.out.println(reply*4);
+    }
 }
-
