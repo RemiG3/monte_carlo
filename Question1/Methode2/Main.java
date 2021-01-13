1d218dcccdc466df7b2c32d1e64ea7c457571d7f
Rémi Gauchotte <remi.gauchotte6@etu.univ-lorraine.fr>
2021-01-13 17:21:34 +0100

    Add method 2

--- Question1/Methode2/Main.java
+++ Question1/Methode2/Main.java
@@ -5,6 +5,26 @@ class Main{
 	
 	public static void main(String[] args){
 		
+		long startTime = System.currentTimeMillis();
+		
+		double c = 0;
+		double a = 0, b = 1;
+		double M = 10;
+		double n = 100_000_000;
+		for(long i = 1; i < n; i++){
+			double xi = Math.random()*(b-a);
+			double yi = Math.random()*M;
+			
+			if(yi <= Math.sqrt((b-a)-Math.pow(xi, 2)))
+				c++;
+		}
+		
+		double result = ((c*(b-a)*M) / n)*4;
+		
+		long endTime = System.currentTimeMillis();
+		
+		System.out.println("Result found in " + Math.round(endTime-startTime) / 1000.0 + " sec.");
+		System.out.println(result);
 	}
 	
 }
